98162c75bd3244facecf439a11c2e90c5dc2b61c
SandroCOCAG <sandro.leidmann@coc-ag.de>
2026-01-08 09:41:19 +0100

    Variablen

--- input.py
+++ input.py
@@ -5,3 +5,10 @@ print("Hello " + name)
 alter = input(int("Wie alt bist du?"))
 
 print(name + " du bist " + int(alter) + " Jahre alt.")
+
+a = Mango
+b = Apfel
+c = Orange
+d = Kirsche
+
+x & z & y = Banane
